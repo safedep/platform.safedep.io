1e2e1eb085a3b8ff976bef54eec42bfe0e5cbe4e
abhisek <abhisek.datta@gmail.com>
2024-12-03 09:53:37 +0530

    refactor: Login page with nextjs conventions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
-import './globals.css';
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
 
-export const metadata = {
-  title: 'SafeDep Platform',
-  description: 'Safedep',
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "SafeDep | Open Source Software Supply Chain Security Platform",
+  description: "Welcome to SafeDep. Onboard to SafeDep cloud, generate authentication credentials and access platform APIs",
 };
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
-};
-
-export default RootLayout;
+}
