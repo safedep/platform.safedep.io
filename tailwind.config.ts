cae23d5f5a0f2b59c557bca167d78ae3c8e5eb71
abhisek <abhisek.datta@gmail.com>
2024-12-03 09:59:18 +0530

    fix: Restore tailwind.config.js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,6 @@
-// import type { Config } from "tailwindcss";
-
-// export default {
-//   content: [
-//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-//   ],
-//   theme: {
-//     extend: {
-//       colors: {
-//         background: "var(--background)",
-//         foreground: "var(--foreground)",
-//       },
-//     },
-//   },
-//   plugins: [],
-// } satisfies Config;
 import type { Config } from "tailwindcss";
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+
+export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -26,6 +8,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
     },
   },
   plugins: [],
