f4a9cb5344b471abe543abb0f264adcaef11cd4b
Kunaldevxxx <khareji594@gmail.com>
2024-12-03 18:59:38 +0530

    Testcase Solved

--- src/app/auth/components/AuthButtons.tsx
+++ src/app/auth/components/AuthButtons.tsx
@@ -9,8 +9,8 @@ const AuthButtons: React.FC = () => {
         href="/api/auth/login"
         className="w-full flex items-center justify-center px-6 py-3 bg-blue-600 text-white rounded-xl hover:bg-blue-700 transition-all shadow-md group focus:ring-2 focus:ring-blue-300 focus:outline-none"
       >
-        <User className="mr-3 group-hover:animate-bounce" />
-        Create Account / Login
+        <User className="mr-3 group-hover:animate-bounce "  />
+        Create Account Or Login
       </Link>
     </div>
   );
