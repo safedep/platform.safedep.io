cb4aa11b21248cfe5915cff91ccb1bbf7d1dabe7
Abhisek Datta <abhisek.datta@gmail.com>
2024-12-04 10:01:54 +0530

    feat: Support separate button for login and signup (#9)

---
+++ src/app/api/auth/[auth0]/route.ts
@@ -0,0 +1,9 @@
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+
+// export Auth0's default handleAuth with custom action for
+// sign-up to render the sign-up screen by default
+export const GET = handleAuth({
+  signup: handleLogin({
+    authorizationParams: { screen_hint: 'signup' },
+  })
+})
