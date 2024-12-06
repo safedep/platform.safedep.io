fb08225728b31694135c6d60adc6506a0f6a448b
abhisek <abhisek.datta@gmail.com>
2024-12-06 10:03:26 +0530

    refactor: Header to use dynamic year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,31 @@
+'use client'
+
+import Link from "next/link";
 import React from "react";
-const Footer: React.FC = () => 
-<footer 
-className="bg-gray-100 text-gray-700 text-sm mt-auto w-full text-center py-4">SafeDep &bull; 
-<a href="#">Advertise</a> &bull;
- <a href="#">Privacy</a></footer>;
+import { useState } from "react";
+
+const Footer: React.FC = () => {
+  const [year, setYear] = useState(new Date().getFullYear());
+
+  useState(() => {
+    setYear(new Date().getFullYear());
+  })
+
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center">
+      <p>&copy; {year} SafeDep</p>
+
+      <p className="text-sm">
+        <Link href="#" className="text-white">
+          Terms of Service
+        </Link>
+        {" | "}
+        <Link href="#" className="text-white">
+          Privacy Policy
+        </Link>
+      </p>
+    </footer>
+  );
+}
 
 export default Footer;
