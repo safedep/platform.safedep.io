95814fe5fac5653540c632b55bec9065006b6516
abhisek <abhisek.datta@gmail.com>
2024-12-12 08:09:17 +0530

    feat: Add API for API key management operations

---
+++ src/app/api/platform/keys/route.ts
@@ -0,0 +1,75 @@
+import { apiErrorHandler } from "@/lib/api/error";
+import { createApiKeyServiceClient } from "@/lib/rpc/client";
+import { CreateApiKeyRequest, CreateApiKeyResponse, DeleteApiKeyRequest, ListApiKeyResponse } from "@/lib/schema/apikey";
+import { createValidationError } from "@/lib/schema/error";
+import { validateSchema } from "@/lib/schema/validate";
+import { sessionMustGetTenant } from "@/lib/session/session";
+import { getAccessToken } from "@auth0/nextjs-auth0";
+import { NextResponse, NextRequest } from "next/server";
+import { z } from "zod";
+
+async function handleListApiKeys() {
+  const { accessToken } = await getAccessToken();
+  const tenant = await sessionMustGetTenant();
+
+  const client = createApiKeyServiceClient(tenant, accessToken as string);
+  const keys = await client.listApiKeys({})
+
+  type responseType = z.infer<typeof ListApiKeyResponse>;
+  const response: responseType = {
+    keys: keys.keys.map((key) => ({
+      id: key.keyId,
+      name: key.name,
+      description: key.description,
+      expiry: key.expiresAt?.toDate(),
+    })),
+    total: keys.keys.length,
+  };
+
+  return NextResponse.json(response);
+}
+
+async function handleCreateApiKey(req: NextRequest) {
+  const { accessToken } = await getAccessToken();
+  const tenant = await sessionMustGetTenant();
+
+  const { validData, errors } = validateSchema(CreateApiKeyRequest, await req.json());
+  if (!validData || errors) {
+    return NextResponse.json(createValidationError(errors), { status: 400 });
+  }
+
+  const client = createApiKeyServiceClient(tenant, accessToken as string);
+  const response = await client.createApiKey({
+    name: validData.name,
+    description: validData.description,
+    expiryDays: validData.expiryDays,
+  })
+
+  type responseType = z.infer<typeof CreateApiKeyResponse>;
+  const responseBody: responseType = {
+    id: response.keyId,
+    key: response.key,
+    expiry: response.expiresAt?.toDate(),
+  };
+
+  return NextResponse.json(responseBody, { status: 201 });
+}
+
+async function handleDeleteApiKey(req: NextRequest) {
+  const { accessToken } = await getAccessToken();
+  const tenant = await sessionMustGetTenant();
+
+  const { validData, errors } = validateSchema(DeleteApiKeyRequest, await req.json());
+  if (!validData || errors) {
+    return NextResponse.json(createValidationError(errors), { status: 400 });
+  }
+
+  const client = createApiKeyServiceClient(tenant, accessToken as string);
+  await client.deleteApiKey({ keyId: validData.id });
+
+  return NextResponse.json({});
+}
+
+export const GET = apiErrorHandler(handleListApiKeys);
+export const POST = apiErrorHandler(handleCreateApiKey);
+export const DELETE = apiErrorHandler(handleDeleteApiKey);
---
+++ src/lib/schema/apikey.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+
+export const ListApiKeyRequest = z.object({
+  page: z.number().optional(),
+  pageSize: z.number().optional(),
+});
+
+export const ListApiKeyResponse = z.object({
+  keys: z.array(
+    z.object({
+      id: z.string(),
+      name: z.string(),
+      description: z.string(),
+      expiry: z.date().optional(),
+    })
+  ),
+  total: z.number(),
+});
+
+export const CreateApiKeyRequest = z.object({
+  name: z.string().min(1, { message: "Name is required" }),
+  description: z.string().min(1, { message: "Description is required" }),
+  expiryDays: z.number()
+    .min(1, { message: "Expiry days is required" })
+    .max(365, { message: "Expiry days must be less than 365" }),
+});
+
+export const CreateApiKeyResponse = z.object({
+  id: z.string().min(1, { message: "Id is required" }),
+  key: z.string().min(1, { message: "Key is required" }),
+  expiry: z.date().optional(),
+});
+
+export const DeleteApiKeyRequest = z.object({
+  id: z.string().min(1, { message: "Id is required" }),
+});
