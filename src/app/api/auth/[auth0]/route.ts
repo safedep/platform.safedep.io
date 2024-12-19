49ed33730429faf35a66c923114e20159703e4b0
Abhisek Datta <abhisek.datta@gmail.com>
2024-12-19 16:55:46 +0530

    fix: Auth0 Unverified Email Handling (#37)
    * feat: Add error handling for authentication and email verification flow
    
    * feat: Refactor AuthError component to use Suspense for loading state
    
    * refactor: Migrate verify-email page within auth
    
    ---------
    
    Co-authored-by: Kunaldevxxx <khareji594@gmail.com>

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,5 +1,5 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
-
+import { NextRequest, NextResponse } from "next/server";
 // export Auth0's default handleAuth with custom action for
 // sign-up to render the sign-up screen by default
 export const GET = handleAuth({
@@ -8,9 +8,34 @@ export const GET = handleAuth({
       screen_hint: 'signup'
     },
   }),
+
   login: handleLogin({
     authorizationParams: {
       scope: 'openid profile email offline_access',
     },
   }),
-})
+
+  // https://github.com/SuhravHussen/UmmahrChintah/blob/bb79e4f97c782ce2572f28afa72c17dad9469ed8/client/app/api/auth/%5Bauth0%5D/route.ts
+  onError: async (req: NextRequest) => {
+    const url = new URL(req.url);
+    const errorType = url.searchParams.get("error");
+    const errorDescription = url.searchParams.get("error_description") || "";
+
+    if (
+      errorType === "access_denied" &&
+      errorDescription?.includes("verify your email")
+    ) {
+      return NextResponse.redirect(
+        new URL(`/auth/verify-email`, process.env.NEXT_PUBLIC_URL || req.url)
+      );
+    }
+
+    return NextResponse.redirect(
+      new URL(
+        `/auth/error?message=${encodeURIComponent(errorDescription)}`,
+        process.env.NEXT_PUBLIC_URL || req.url
+      )
+    );
+  },
+});
+
