b73d5b472fd4dd91abe55e39dc64f62eb0e6f932
abhisek <abhisek.datta@gmail.com>
2025-01-05 20:58:01 +0530

    fix: jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -10,11 +10,6 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
-  verbose: true,
-  transformIgnorePatterns: [
-    "node_modules/(?!(@buf)/)",
-    "node_modules/(?!(@bufbuild)/)",
-  ],
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
