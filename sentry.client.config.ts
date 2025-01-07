a6bfbfdb359ad1bf42a0a2d73b61b420da350af9
abhisek <abhisek.datta@gmail.com>
2025-01-07 09:04:51 +0530

    chore: Sentry enable user feedback system

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -11,7 +11,14 @@ Sentry.init({
     process.env.NEXT_PUBLIC_ENV === "production",
 
   // Add optional integrations for additional features
-  integrations: [Sentry.replayIntegration()],
+  integrations: [
+    Sentry.replayIntegration(),
+    Sentry.feedbackIntegration({
+      colorScheme: "system",
+      isNameRequired: true,
+      isEmailRequired: true,
+    }),
+  ],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
