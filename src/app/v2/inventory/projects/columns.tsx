f4b2fcc90691d25a518140059fb957af73756de3
Arunanshu Biswas <mydellpc07@gmail.com>
2025-01-28 11:17:05 +0530

    feat: add action button

--- src/app/v2/inventory/projects/columns.tsx
+++ src/app/v2/inventory/projects/columns.tsx
@@ -1,7 +1,21 @@
 "use client";
 
-import type { Project_Source } from "@buf/safedep_api.bufbuild_es/safedep/messages/controltower/v1/project_pb";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Project_Source } from "@buf/safedep_api.bufbuild_es/safedep/messages/controltower/v1/project_pb";
+import {
+  SiBitbucket,
+  SiGithub,
+  SiGitlab,
+} from "@icons-pack/react-simple-icons";
 import type { ColumnDef } from "@tanstack/react-table";
+import { Box, MoreHorizontal, ScrollText } from "lucide-react";
 
 export interface Project {
   id: string;
@@ -34,10 +48,75 @@ export const columns: ColumnDef<Project>[] = [
   {
     accessorKey: "source",
     header: "Source",
+    cell: ({ row }) => {
+      return (
+        <div className="flex gap-2">
+          <div>{sourceToIcon(row.original.source)}</div>
+          <div>{sourceToName(row.original.source)}</div>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "createdAt",
     header: "Created At",
-    cell: ({ row }) => row.original.createdAt.toLocaleDateString(),
+    cell: ({ row }) => row.original.createdAt.toISOString(),
+  },
+
+  {
+    accessorKey: "actions",
+    header: "",
+    cell: ({ row }) => {
+      const key = row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => navigator.clipboard.writeText(key.id)}
+            >
+              Copy Key ID
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
+
+function sourceToIcon(source: Project_Source) {
+  switch (source) {
+    case Project_Source.GITHUB:
+      return <SiGithub className="h-4 w-4" />;
+    case Project_Source.GITLAB:
+      return <SiGitlab className="h-4 w-4" />;
+    case Project_Source.BITBUCKET:
+      return <SiBitbucket className="h-4 w-4" />;
+    case Project_Source.OTHER:
+      return <Box className="h-4 w-4" />;
+    default:
+      return <ScrollText className="h-4 w-4" />;
+  }
+}
+
+function sourceToName(source: Project_Source) {
+  switch (source) {
+    case Project_Source.GITHUB:
+      return "GitHub";
+    case Project_Source.GITLAB:
+      return "GitLab";
+    case Project_Source.BITBUCKET:
+      return "Bitbucket";
+    case Project_Source.OTHER:
+      return "Other";
+    default:
+      return "Unknown";
+  }
+}
