bbab2ee7da95b013d7f1c83a88aa4cfa5d567f76
Arunanshu Biswas <mydellpc07@gmail.com>
2025-02-12 17:18:29 +0530

    feat: fix tags-input type errors

--- src/app/v2/policy/new-policy/page.tsx
+++ src/app/v2/policy/new-policy/page.tsx
@@ -2,7 +2,11 @@ import CreatePolicyForm from "@/components/policy/create-policy-form";
 
 export default function Page() {
   return (
-    <div>
+    <div className="flex flex-col gap-6">
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold tracking-tight">Create New Policy</h1>
+      </div>
+
       <CreatePolicyForm />
     </div>
   );
