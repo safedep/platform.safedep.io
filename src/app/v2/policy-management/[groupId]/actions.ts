0e83952264eded17158f6dade4bdb477240a231e
Arunanshu Biswas <mydellpc07@gmail.com>
2025-02-19 09:51:17 +0530

    feat: add attach/detatch server actions

--- src/app/v2/policy-management/[groupId]/actions.ts
+++ src/app/v2/policy-management/[groupId]/actions.ts
@@ -20,6 +20,50 @@ async function getTenantAndToken() {
 export async function getPolicyGroup(groupId: string) {
   const { accessToken, tenant } = await getTenantAndToken();
   const policyServiceClient = createPolicyService(tenant, accessToken);
-  const policyGroup = await policyServiceClient.getPolicyGroup({ groupId });
-  return policyGroup;
+  const { policies, group } = await policyServiceClient.getPolicyGroup({
+    groupId,
+  });
+  return {
+    group: {
+      id: group?.policyGroupId,
+      name: group?.name,
+      description: group?.description,
+      createdAt: group?.createdAt?.toDate(),
+      updatedAt: group?.updatedAt?.toDate(),
+    },
+    policies: policies.map(({ labels, name, policyId, rules, target }) => ({
+      id: policyId,
+      name,
+      target,
+      labels,
+      rules: rules.map(
+        ({ description, name, check, labels, references, value }) => ({
+          description,
+          name,
+          check,
+          labels,
+          references,
+          value,
+        }),
+      ),
+    })),
+  };
+}
+
+export async function attachPolicyToGroup(groupId: string, policyId: string) {
+  const { accessToken, tenant } = await getTenantAndToken();
+  const policyServiceClient = createPolicyService(tenant, accessToken);
+  await policyServiceClient.attachPolicyToGroup({
+    policyGroupId: groupId,
+    policyId,
+  });
+}
+
+export async function detachPolicyFromGroup(groupId: string, policyId: string) {
+  const { accessToken, tenant } = await getTenantAndToken();
+  const policyServiceClient = createPolicyService(tenant, accessToken);
+  await policyServiceClient.detachPolicyFromGroup({
+    policyGroupId: groupId,
+    policyId,
+  });
 }
