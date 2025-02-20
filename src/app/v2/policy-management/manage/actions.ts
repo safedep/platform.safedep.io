5183500ea58058533657f48a1482451dbdf0ccd1
Arunanshu Biswas <mydellpc07@gmail.com>
2025-02-20 16:26:54 +0530

    feat: add createdAt and updatedAt timestamps to policy groups

--- src/app/v2/policy-management/manage/actions.ts
+++ src/app/v2/policy-management/manage/actions.ts
@@ -7,11 +7,15 @@ export async function getPolicyGroups() {
   const { accessToken, tenant } = await getTenantAndToken();
   const policyServiceClient = createPolicyService(tenant, accessToken);
   const { groups } = await policyServiceClient.listPolicyGroups({});
-  return groups.map(({ name, policyGroupId, description }) => ({
-    id: policyGroupId,
-    name,
-    description,
-  }));
+  return groups.map(
+    ({ name, policyGroupId, description, createdAt, updatedAt }) => ({
+      id: policyGroupId,
+      name,
+      description,
+      createdAt: createdAt?.toDate(),
+      updatedAt: updatedAt?.toDate(),
+    }),
+  );
 }
 
 export async function deletePolicyGroup(groupId: string) {
