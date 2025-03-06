3d7dd13bdb4fb9003278e3cadac56848b5e5f400
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2025-03-06 12:28:46 +0530

    fix: disable logout until tenant redirect happens (#106)
    * feat: disable logout until tenant redirect happens
    
    Signed-off-by: Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
    
    * fix edge case when tenant selection fails
    
    Signed-off-by: Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
    
    * remove duplicate function layer
    
    Signed-off-by: Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
    
    ---------
    
    Signed-off-by: Sudhanshu Dasgupta <dasguptashivam23@gmail.com>

---
+++ src/components/tenant-selector.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+interface UserInfo {
+  access: { tenant?: { domain: string } }[];
+}
+
+interface TenantSelectorProps {
+  userInfo: UserInfo;
+  handleSetTenant: (tenant: string) => void;
+  handleLogout: () => void;
+}
+
+export default function TenantSelector({
+  userInfo,
+  handleSetTenant,
+  handleLogout,
+}: TenantSelectorProps) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <Card className="w-full max-w-md">
+      <CardHeader>
+        <CardTitle>Select Tenant</CardTitle>
+        <CardDescription>
+          Select the tenant for use with the application
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid w-full items-center gap-4">
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="tenant">Tenant</Label>
+            <Select
+              name="tenant"
+              onValueChange={async (tenant) => {
+                setIsLoading(true);
+                try {
+                  await handleSetTenant(tenant);
+                } finally {
+                  setIsLoading(false);
+                }
+              }}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Select tenant to continue ..." />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                {userInfo?.access.map((access) => (
+                  <SelectItem
+                    key={access?.tenant?.domain}
+                    value={access?.tenant?.domain ?? ""}
+                  >
+                    {access.tenant?.domain}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline" onClick={handleLogout} disabled={isLoading}>
+          Logout
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
