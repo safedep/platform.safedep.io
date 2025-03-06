f4e35bdb244c48025a830f07812ae8ea5b56d2c3
Arunanshu Biswas <mydellpc07@gmail.com>
2025-03-06 21:00:37 +0530

    fix: logout link

--- src/components/LogoutLink.tsx
+++ src/components/LogoutLink.tsx
@@ -7,15 +7,13 @@ export interface LogoutLinkProps {
   children?: React.ReactNode;
 }
 
-export const LogoutLink: React.FC<LogoutLinkProps> = (
-  props: LogoutLinkProps,
-) => {
+export function LogoutLink(props: LogoutLinkProps) {
   return (
     // We must use <a> tag for logout link to prevent Next.js from prefetching the page
     // https://github.com/auth0/nextjs-auth0/issues/757
     // eslint-disable-next-line @next/next/no-html-link-for-pages
-    <a href="/api/auth/logout" className={props.className}>
+    <a href="/auth/logout" className={props.className}>
       {props.children}
     </a>
   );
-};
+}
