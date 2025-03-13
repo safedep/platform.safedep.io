18e24cd78068fea375e99dca0fadbaea290a3330
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2025-03-13 15:48:26 +0530

    remove unused sections and components
    Signed-off-by: Sudhanshu Dasgupta <dasguptashivam23@gmail.com>

--- src/app/v2/policy/violations/actions.ts
+++
@@ -1,8 +0,0 @@
-import { createPolicyService } from "@/lib/rpc/client";
-import { getTenantAndToken } from "@/lib/session/session";
-
-export async function listPolicyViolations() {
-  const { tenant, accessToken } = await getTenantAndToken();
-  const service = createPolicyService(tenant, accessToken);
-  return await service.listPolicyViolation({});
-}
