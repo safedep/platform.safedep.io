3faf02e7ad635e6929a9ef231c40e7e75dce0352
Arunanshu Biswas <mydellpc07@gmail.com>
2025-04-23 15:13:56 +0530

    clear everything

--- setupFiles.ts
+++
@@ -1,5 +0,0 @@
-/**
- * This setup file is used to configure the testing environment for the project.
- * It is specified in the `setupFiles` configuration option of Vitest.
- */
-import "@testing-library/jest-dom";
--- src/app/(app)/onboard/loading.tsx
+++
@@ -1,6 +0,0 @@
-import Loading from "@/components/Loading";
-import { TimerIcon } from "lucide-react";
-
-export default function LoadingPage() {
-  return <Loading message="Loading..." badge={TimerIcon} />;
-}
