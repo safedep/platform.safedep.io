3eb0cc3a2bd436c29b0275c46e798aef6c4be28f
Arunanshu Biswas <mydellpc07@gmail.com>
2025-04-25 03:21:31 +0530

    add test for auth page

---
+++ src/app/(auth)/auth/page.test.tsx
@@ -0,0 +1,32 @@
+import { describe, it, expect } from "vitest";
+import AuthPage from "./page";
+import { render, screen } from "@testing-library/react";
+
+describe("Auth Page", () => {
+  it("should render the page", () => {
+    // Arrange
+    render(<AuthPage />);
+
+    // Assert
+    expect(screen.getByText("Welcome to SafeDep")).toBeInTheDocument();
+    expect(screen.getByText("Create Account")).toBeInTheDocument();
+    expect(screen.getByText("Login")).toBeInTheDocument();
+  });
+
+  it("has links to the login and signup pages", () => {
+    // Arrange
+    render(<AuthPage />);
+
+    // Assert
+    const loginLink = screen.getByRole("link", { name: /login/i });
+    expect(loginLink).toBeInTheDocument();
+    expect(loginLink).toHaveAttribute("href", "/auth/login");
+
+    const signupLink = screen.getByRole("link", { name: /create account/i });
+    expect(signupLink).toBeInTheDocument();
+    expect(signupLink).toHaveAttribute(
+      "href",
+      "/auth/login?screen_hint=signup",
+    );
+  });
+});
