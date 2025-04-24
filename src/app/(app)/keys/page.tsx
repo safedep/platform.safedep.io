cf63b0de38141a3965e9ca5014c45e62c514bedb
Arunanshu Biswas <mydellpc07@gmail.com>
2025-04-24 17:13:31 +0530

    page title for keys page

--- src/app/(app)/keys/page.tsx
+++ src/app/(app)/keys/page.tsx
@@ -1,6 +1,12 @@
 import { sessionGetTenant, sessionRequireAuth } from "@/lib/session/session";
 import KeysClient from "./client";
 import { redirect } from "next/navigation";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "API Keys | SafeDep",
+  description: "Manage your API keys",
+};
 
 export default async function KeysPage() {
   await sessionRequireAuth();
