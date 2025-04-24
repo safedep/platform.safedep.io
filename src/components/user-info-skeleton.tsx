44af23265f4957e5062f9e822e571b882579dcb4
Arunanshu Biswas <mydellpc07@gmail.com>
2025-04-24 16:34:38 +0530

    fix skeleton width

--- src/components/user-info-skeleton.tsx
+++ src/components/user-info-skeleton.tsx
@@ -10,7 +10,7 @@ interface UserInfoSkeletonProps {
 
 export default function UserInfoSkeleton({ className }: UserInfoSkeletonProps) {
   return (
-    <Card className={cn("min-w-sm", className)}>
+    <Card className={cn("lg:min-w-sm", className)}>
       <CardHeader>
         <CardTitle className="flex items-center justify-between text-lg font-semibold">
           <span>User Information</span>
