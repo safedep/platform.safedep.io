2e158e07cb9b14fc80a7e024daf492897bfc3350
Arunanshu Biswas <mydellpc07@gmail.com>
2025-04-26 02:02:02 +0530

    add test for keys list component

--- src/app/(app)/keys/page.test.tsx
+++ src/app/(app)/keys/page.test.tsx
@@ -134,4 +134,81 @@ describe("Keys Page", () => {
     expect(screen.getByText("John Doe")).toBeInTheDocument();
     expect(screen.getByText("john.doe@example.com")).toBeInTheDocument();
   });
+
+  it("should display API keys", async () => {
+    // Arrange
+    // set response for api keys listing action
+    mocks.actions.getApiKeys.mockResolvedValue({
+      tenant: "some-tenant",
+      apiKeys: [
+        {
+          id: "1",
+          description: "description of my-api-key",
+          expiresAt: new Date("2025-05-01"),
+          name: "my-api-key",
+        },
+        {
+          id: "2",
+          description: "", // will be displayed as "—"
+          expiresAt: new Date("2025-05-02"),
+          name: "my-api-key-2",
+        },
+      ],
+    } satisfies ApiKeys);
+    // set response for user info action
+    mocks.actions.getUserInfo.mockResolvedValue({
+      userInfo: {
+        name: "John Doe",
+        email: "john.doe@example.com",
+        avatar: "https://example.com/avatar.png",
+      },
+      tenants: [],
+      currentTenant: "some-tenant",
+    } satisfies UserInfo);
+    // current tenant
+    mocks.session.sessionGetTenant.mockResolvedValue("some-tenant");
+
+    // Act
+    const { page, queryClient } = await setupPageComponent();
+    render(page);
+    await waitFor(() => {
+      // wait for the query client to resolve all queries
+      expect(queryClient.isFetching()).toBe(0);
+    });
+
+    // Assert
+    const table = screen.getByRole("table");
+    expect(table).toBeInTheDocument();
+
+    // get columns
+    const columns = screen.getAllByRole("columnheader");
+    expect(columns).toHaveLength(5);
+    expect(columns[0]).toHaveTextContent("ID");
+    expect(columns[1]).toHaveTextContent("Name");
+    expect(columns[2]).toHaveTextContent("Description");
+    expect(columns[3]).toHaveTextContent("Expires At");
+    expect(columns[4]).toHaveTextContent(""); // actions column
+
+    // get rows
+    const rows = screen.getAllByRole("row");
+    expect(rows).toHaveLength(3); // 0-th row is the header
+
+    // get cells for the first row
+    const row = rows[1]; // 0-th row is the header
+    const cells = row.querySelectorAll("td");
+    expect(cells).toHaveLength(5);
+    expect(cells[0]).toHaveTextContent("1");
+    expect(cells[1]).toHaveTextContent("my-api-key");
+    expect(cells[2]).toHaveTextContent("description of my-api-key");
+    // TODO: how to test the date in locale compatible way?
+
+    // get cells for the second row
+    const row2 = rows[2]; // 0-th row is the header
+    const cells2 = row2.querySelectorAll("td");
+    expect(cells2).toHaveLength(5);
+    expect(cells2[0]).toHaveTextContent("2");
+    expect(cells2[1]).toHaveTextContent("my-api-key-2");
+    expect(cells2[2]).toHaveTextContent("—");
+    // TODO: how to test the date in locale compatible way?
+  });
 });
