3384c28964eb6f59e8346509100014bc6b7bbf9e
Arunanshu Biswas <mydellpc07@gmail.com>
2025-04-26 11:04:08 +0530

    title for onboarding page

--- src/app/(app)/onboard/page.tsx
+++ src/app/(app)/onboard/page.tsx
@@ -2,6 +2,11 @@ import OnboardClient from "./client";
 import AlreadyOnboardedDialog from "@/components/onboarding/already-onboarded-dialog";
 import { sessionRequireAuth } from "@/lib/session/session";
 import { isUserOnboarded } from "./actions";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Onboarding | Safedep Platform",
+};
 
 export default async function Page() {
   const isOnboarded = await isUserOnboarded();
