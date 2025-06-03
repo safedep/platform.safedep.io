5e1e0a36c4fce673680498f29516eff9861e1ef1
Arunanshu Biswas <mydellpc07@gmail.com>
2025-06-03 13:01:00 +0530

    use vitest specific dom matchers

--- setupFiles.ts
+++ setupFiles.ts
@@ -2,4 +2,4 @@
  * This setup file is used to configure the testing environment for the project.
  * It is specified in the `setupFiles` configuration option of Vitest.
  */
-import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/vitest";
