dc84ea4884ae5f38bfd8a3fdee71b444e4603a39
Arunanshu Biswas <mydellpc07@gmail.com>
2025-06-03 13:13:28 +0530

    fix vitest timers

--- setupFiles.ts
+++ setupFiles.ts
@@ -3,3 +3,12 @@
  * It is specified in the `setupFiles` configuration option of Vitest.
  */
 import "@testing-library/jest-dom/vitest";
+import { vi } from "vitest";
+
+// User event and testing-library expect jest be available. We know that vitest
+// is jest compatible so we stub it.
+// https://github.com/testing-library/user-event/issues/1115#issuecomment-2495876991
+// https://github.com/testing-library/user-event/issues/1115#issuecomment-2495876991
+vi.stubGlobal("jest", {
+  advanceTimersByTime: vi.advanceTimersByTime.bind(vi),
+});
