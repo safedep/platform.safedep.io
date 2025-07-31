916c2605754ca4dd1fca108a582c589098048a33
Arunanshu Biswas <mydellpc07@gmail.com>
2025-07-31 21:18:14 +0530

    fix posthog

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -12,9 +12,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Auth0Provider>
       <TanstackQueryProvider>
-        {/* <PostHogProvider> */}
-        {children}
-        {/* </PostHogProvider> */}
+        <PostHogProvider>{children}</PostHogProvider>
       </TanstackQueryProvider>
     </Auth0Provider>
   );
