55d3d6d8e744d722b68431ead1d778978ab3a9d1
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2025-09-12 11:46:45 +0530

    feat: github resume linking flow (#191)
    * feat: github resume linking flow
    
    * explicit env variable
    
    * fix test
    
    * fix integration test
    
    * fix redirection params

--- src/app/(app)/connect/github/page.tsx
+++ src/app/(app)/connect/github/page.tsx
@@ -4,9 +4,12 @@ import ConnectGithubClient from "./client";
 import { redirect } from "next/navigation";
 import OauthConnect from "@/components/oauth-connector/oauth-connect";
 import { SiGithub } from "@icons-pack/react-simple-icons";
+import { oauthAuthorizationUrl } from "@octokit/oauth-authorization-url";
+import { env } from "@/env";
+import { Route } from "next";
 
 const searchParamSchema = v.object({
-  code: v.pipe(v.string(), v.nonEmpty()),
+  code: v.optional(v.pipe(v.string(), v.nonEmpty())),
   installation_id: v.pipe(
     v.string(),
     v.nonEmpty(),
@@ -16,6 +19,26 @@ const searchParamSchema = v.object({
   setup_action: v.optional(v.string()),
 });
 type SearchParams = v.InferInput<typeof searchParamSchema>;
+
+function buildRedirectUrl(
+  clientId: string,
+  installationId: number,
+  setupAction?: string,
+) {
+  const returnToUrl = new URL("/connect/github", env.APP_BASE_URL);
+  returnToUrl.searchParams.set("installation_id", installationId.toString());
+  if (setupAction) {
+    returnToUrl.searchParams.set("setup_action", setupAction);
+  }
+
+  const url = oauthAuthorizationUrl({
+    clientType: "oauth-app",
+    clientId,
+    redirectUrl: returnToUrl.toString(),
+    scopes: ["user:email"], // only for OAuth Apps
+  });
+  return url.url;
+}
 
 export default async function ConnectGithubPage({
   searchParams,
@@ -35,6 +58,18 @@ export default async function ConnectGithubPage({
     installation_id: installationId,
     setup_action: setupAction,
   } = output;
+
+  // we assume if the code is absent, it represents the fact that user has not yet finished linking
+  // the app to a tenant. For that to happen, we need the code to act on behalf of the user.
+  if (!code) {
+    const redirectUrl = buildRedirectUrl(
+      env.GITHUB_APP_INTEGRATION_CLIENT_ID,
+      installationId,
+      setupAction,
+    );
+    return redirect(redirectUrl as Route);
+  }
+
   const { email, tenants } = await getUserInfoOrRedirectToAuth(
     encodeURIComponent(
       `/connect/github?code=${code}&installation_id=${installationId}&setup_action=${setupAction}`,
